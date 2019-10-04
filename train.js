9865c7f791f49217d3081247fbbf4e8df04bf345
Danger <51889207+DangerDan88@users.noreply.github.com>
2019-10-03 18:10:13 -0600

    train logic mainly done need to fix somethings and style more

--- train.js
+++ train.js
@@ -1,5 +1,5 @@
 $(function () {
-
+        // firebase config key to get connected
     var firebaseConfig = {
         apiKey: "AIzaSyBBpAssZZODfjKJjV6Le3QyLAhXcwI2Hnw",
         authDomain: "dan-train.firebaseapp.com",
@@ -9,42 +9,73 @@ $(function () {
         messagingSenderId: "1081380486863",
         appId: "1:1081380486863:web:6daf268e368981f59881ba"
     };
-
+    // initializes fire base
     firebase.initializeApp(firebaseConfig);
     var database = firebase.database();
+    var trainName = "";
+    var destination = "";
+    var frequency = 0;
+    var startTime = 0;
 
     $("#add-train-btn").on('click', function () {
         event.preventDefault();
-        var trainName = $("#train-name-input").val().trim();
-        var destination = $("#place-input").val().trim();
-        var frequency = $("#time-input").val().trim();
-        var startTime = $("#start-input").val().trim();
-        console.log(trainName);
-        console.log(destination);
-        console.log(frequency);
-        console.log(startTime);
-        database.ref().push({
+        trainName = $("#train-name-input").val().trim();
+        destination = $("#place-input").val().trim();
+        frequency = $("#time-input").val().trim();
+        startTime = $("#start-input").val().trim();
+
+
+            // saved this data as an object to easily push to firebase.
+        var newTrain = {
             trainName: trainName,
             destination: destination,
             frequency: frequency,
-            startTime: startTime
-        })
+            startTime: startTime,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        };
 
+        database.ref().push(newTrain);
+
+        $("#train-name-input").val("");
+        $("#place-input").val("");
+        $("#time-input").val("");
+        $("#start-input").val("");
 
 
     })
 
- database.ref().on("child_added", function (snapshot) {
- var sv = snapshot.val();
-
-$("<tbody>").append("<tr>").append($("<td>").text(sv.trainName));
-
-
-console.log(snapshot.val());
-
+    database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+            // this catches user input to firbase and is used in these variables.
+        var trainName = childSnapshot.val().trainName;
+        var destination = childSnapshot.val().destination;
+        var startTime = childSnapshot.val().startTime;
+        var frequency = childSnapshot.val().frequency;
 
 
+        
 
+
+        var firstTime = startTime;
+
+            // start back in time so you get a positive number back for minutes away
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+
+            // this gets current time.
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+            console.log(diffTime);
+        var remainder = diffTime % parseInt(frequency);
+        console.log(remainder);
+
+        var minutesToTrain = frequency - remainder;
+        var nextTrain = moment().add(minutesToTrain, "minutes");
+
+        var nextTrain = moment().add(minutesToTrain, "minutes");
+        $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency +
+            "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + minutesToTrain + "</td></tr>");
+
+       
 
     })
 })
